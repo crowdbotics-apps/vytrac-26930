05c9261a99a7637f1c9cbf2603d0082f70769793
Jesus Viloria <jesusviloria@Jesuss-MacBook-Air.local>
2021-07-03 22:57:47 -0400

    connected to the backenmd for login

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,11 +5,48 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import './App.css';
 
+import {store} from './util/store'
+
 import loginBackground from './assets/images/login_background.png';
 import poweredBy from './assets/images/powered_by.png';
 import vytracWhite from './assets/images/vytrac_white.png';
 
 class App extends Component {
+
+  constructor(props) {
+    store.init();
+    super(props);
+    this.state = {}
+  }
+
+  validate () {
+    if (!this.state.username) {
+      alert("Please fill your username");
+      return false;
+    } else if (!this.state.password) {
+      alert("Please fill your password");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  login() { 
+    if(this.validate()) {
+      try {
+        store.data.login(this.state.username, this.state.password).then((result) => {
+          if(result) {
+            alert("Successful login!");
+          } else {
+            alert("Could not login");
+          }
+        });
+      } catch (ex) {
+        alert("Could not login");
+      }
+    }
+  }
+
   render() {
     return (
       <div className="login-background"> 
@@ -37,8 +74,8 @@ class App extends Component {
               <p className="text-left smaller-text">Lorem ipsum dolor sit amet, cons cdipiscing elit. Duis non turpis nec nunc </p>
 
               <p className="grey-text username-label">Username, Email or Phone number</p>
-              <input className="text-field" type="text" name="username" />
-              <input className="text-field" type="text" name="password" />
+              <input onChange={(username) => this.setState({username: username})} className="text-field" type="text" name="username" />
+              <input onChange={(password) => this.setState({password: password})} className="text-field" type="password" name="password" />
 
               <Row className="vertical-container remember-me">
                 <Col style={{padding: 0, margin: 0}}  xs={1}>
@@ -51,7 +88,7 @@ class App extends Component {
 
               <div className="login-bottom-buttons-container">
                 <Button style={{marginRight: 50}} variant="light">FORGOT PASSWORD</Button>
-                <Button>LOGIN</Button>
+                <Button onClick={() => this.login()}>LOGIN</Button>
               </div>
             </Col>
           </Row>
---
+++ webapp/src/util/store.js
@@ -0,0 +1,66 @@
+const backendUrl = 'https://vytrac-26930.botics.co/';
+
+export class store {
+
+    static data = undefined;
+
+    static init(props){
+        if(!this.data) {
+            this.data = new StoreConfig();
+            this.data.init(props);
+            return this.data;
+        } else {
+            return this.data;
+        }
+    }
+
+}
+
+class StoreConfig {
+
+    userId = undefined;
+    username = undefined;
+    name = undefined;
+    token = undefined;
+    guest = true;
+    requestHandler = undefined;
+    firebaseToken = undefined;
+
+    init(props) {
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////// AUTH ////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    async login(username, password) {
+        var _this = this;
+        return fetch(backendUrl + "login/", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: username,
+                password: password,
+                rememberMe: true
+            })
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                if(responseJson && responseJson.token) {
+                    _this.token = responseJson.token;
+                    console.log('tokens', _this.token);
+                    return true;
+                } else {
+                    return false;
+                }           
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+}
