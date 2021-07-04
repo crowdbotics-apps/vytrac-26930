21378b6288eb5c8b27010ea722919bef4ea558cf
Jesus Viloria <jesusviloria@Jesuss-MacBook-Air.local>
2021-07-03 22:36:23 -0400

    login form completed, needs backend interaction now

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <div className="login-background"> 
         <Container className="login-container">
           <Row className="login-row">
-            <Col className="login-background-image-gradient" xs={8}>
+            <Col className="login-background-image-gradient" xs={7}>
               <img className="login-background-image" src={loginBackground}></img>
               <div className="login-user-thumb"></div>
               <div className="logos-container">
@@ -23,7 +23,7 @@ class App extends Component {
                 <img className="login-logo-bottom" src={vytracWhite}></img>
               </div>              
             </Col>
-            <Col className="login-form" xs={4}>
+            <Col className="login-form" xs={5}>
               <Row className="vertical-container">
                 <Col style={{padding: 0, margin: 0}}>
                   <div className="green-line"></div>
@@ -33,8 +33,26 @@ class App extends Component {
                 </Col>
               </Row>  
               <p className="text-left">Welcome to <span className="bold-blue">VyTrac</span></p>
+
               <p className="text-left smaller-text">Lorem ipsum dolor sit amet, cons cdipiscing elit. Duis non turpis nec nunc </p>
-              <Button>LOGIN</Button>
+
+              <p className="grey-text username-label">Username, Email or Phone number</p>
+              <input className="text-field" type="text" name="username" />
+              <input className="text-field" type="text" name="password" />
+
+              <Row className="vertical-container remember-me">
+                <Col style={{padding: 0, margin: 0}}  xs={1}>
+                  <div className="greycheck" />
+                </Col>
+                <Col style={{padding: 0, margin: 0}}>
+                  <span className="grey-text">&nbsp;&nbsp;&nbsp;&nbsp;Remember me</span>
+                </Col>
+              </Row>  
+
+              <div className="login-bottom-buttons-container">
+                <Button style={{marginRight: 50}} variant="light">FORGOT PASSWORD</Button>
+                <Button>LOGIN</Button>
+              </div>
             </Col>
           </Row>
         </Container>
